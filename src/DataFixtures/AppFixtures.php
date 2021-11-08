b115d1b76332deaf3be72bc79e57155beb95d20b
cdiot <christopher.diot5@gmail.com>
2021-11-08 16:56:34 +0100

    Phone entity + fixture + test

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Phone;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // create 12 phones!
+        for ($i = 1; $i < 12; $i++) {
+            $phone = new Phone();
+            $phone->setName('Iphone ' . $i);
+            $phone->setBrand('Iphone');
+            $phone->setModel('Pro');
+            $phone->setPrice(999.99);
+            $phone->setDescription('Le top du top toujours plus chère pour une qualité égalé.');
+            $manager->persist($phone);
+        }
+
+        $manager->flush();
+    }
+}
